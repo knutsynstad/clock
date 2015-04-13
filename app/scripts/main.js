bb09f5b245963c385df3d75f3758b7637b878d9c
Knut Synstad <knut@synstad.net>
2015-04-13 02:47:16 +0200

    updated arms, started JS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,18 +3,40 @@ console.log('\'Allo \'Allo!');
 
 
 
-var currentSecond = 0;
-var currentMinute = 0;
-var currentHour = 0;
+var currentSecond = 1;
+var currentMinute = 55;
+var currentHour = 1;
 
 var second = document.getElementById('second');
+var minute = document.getElementById('minute');
+var hour = document.getElementById('hour');
 
 function addSecond() {
-	if (currentSecond > 354) {
-		currentSecond = 0;
+	if (currentSecond > 60) {
+		currentSecond = 1;
+		addMinute();
 	}
-	second.setAttribute("transform", "rotate(" + currentSecond + " 50 50)");
-	currentSecond += 6;
+	second.setAttribute("transform", "rotate(" + (currentSecond * 6) + " 50 50)");
+	currentSecond += 1;
 }
 
-setInterval(addSecond, 250);
+function addMinute() {
+	if (currentMinute > 60) {
+		currentMinute = 1;
+		addHour();
+	}
+	minute.setAttribute("transform", "rotate(" + (currentMinute * 6) + " 50 50)");
+	currentMinute += 1;
+}
+
+function addHour() {
+	if (currentHour > 12) {
+		currentHour = 1;
+	}
+	hour.setAttribute("transform", "rotate(" + (currentHour * 30) + " 50 50)");
+	currentHour += 1;
+}
+
+
+
+setInterval(addSecond, 10);
